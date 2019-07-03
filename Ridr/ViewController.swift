3ea5584c1a9acecfe385980e5474cb297f149e4f
mkanoria <mayankkanoria@gmail.com>
2019-07-03 19:07:20 -0400

    First commit

---
+++ Ridr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ridr
+//
+//  Created by CARFAX Ca on 2019-07-03.
+//  Copyright © 2019 CARFAX Ca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
